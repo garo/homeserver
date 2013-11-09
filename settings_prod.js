3ef110d8690d457439ffc31b8e235ba1e7450c53
Juho Mäkinen <juho.makinen@applifier.com>
2013-11-09 19:33:38 +0200

    alarmservice

--- settings_prod.js
+++ settings_prod.js
@@ -1,3 +1,5 @@
+var homeserver = require('./lib/homeserver');
+
 var settings = {};
 
 settings.mysql = {
@@ -8,6 +10,26 @@ settings.statefile = '/state/homeserver.state.json';
 settings.energypump = {
   athomerelaypin : 0
 };
+
+settings.alarms = {
+  "0" : { name : "Olohuone", triggeron : "low", extracheck : motionSensorCheck },
+  "1" : { name : "Apukeittiš", triggeron : "low", extracheck : motionSensorCheck },
+  "2" : { name : "Eteinen", triggeron : "low", extracheck : motionSensorCheck },
+  "3" : { name : "Parvi", triggeron : "low", extracheck : motionSensorCheck },
+  "4" : { name : "Energiapumppu", triggeron : "high" }
+};
+
+// Business logic: Don't alert on motion sensors when we're at home
+function motionSensorCheck(cb) {
+  homeserver.services.energypump.getAtHomeState(function (err, athome) {
+    if (athome) {
+      // Dont alert on motion sensors when we're at home
+      cb(null, false);
+    } else {
+      cb(null, true);
+    }
+  });
+}
 
 exports.get = function (key) {
   return settings[key];
--- settings_test.js
+++ settings_test.js
@@ -10,7 +10,25 @@ settings.energypump = {
   athomerelaypin : 1
 };
 
+settings.alarms = {
+  "0" : { name : "Olohuone", triggeron : "low", extracheck : motionSensorCheck },
+  "1" : { name : "Apukeittiš", triggeron : "low", extracheck : motionSensorCheck },
+  "2" : { name : "Eteinen", triggeron : "low", extracheck : motionSensorCheck },
+  "3" : { name : "Parvi", triggeron : "low", extracheck : motionSensorCheck },
+  "4" : { name : "Energiapumppu", triggeron : "high" }
+};
 
+// Business logic: Don't alert on motion sensors when we're at home
+function motionSensorCheck(cb) {
+  homeserver.services.energypump.getAtHomeState(function (err, athome) {
+    if (athome) {
+      // Dont alert on motion sensors when we're at home
+      cb(null, false);
+    } else {
+      cb(null, true);
+    }
+  });
+}
 exports.get = function (key) {
   return settings[key];
 };
